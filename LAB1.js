13a7a4b289b519acaf8557ddd64ad1cd02d46d47
PRE808 <priyaupple@gmail.com>
2024-10-04 15:28:36 -0500

    Add files via upload

---
+++ LAB1.js
@@ -0,0 +1,11 @@
+console.log(24)
+console.log(1)
+console.log('priya');
+FileSystem.out.printIn(!(false));
+FileSystem.out.printIn( (!6>=1) );
+let firstName=('priya')
+let lastName=('Upple')
+console.log(firstName);
+console.log(lastName);
+console.log('my'+'name');
+console.log(2+3);
